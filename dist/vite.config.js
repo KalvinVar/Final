ea1a16258811573ae43a9212d317cb8289be244d
Kalvin <105901141+KalvinVar@users.noreply.github.com>
2024-06-09 19:38:18 -0700

    Delete dist directory

--- dist/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/Final/finaltest/', // Set the base path to match your GitHub Pages URL
-  plugins: [react()],
-});
